559a7dc579cfefcf0737b2630627acf0665bcaac
Angel Lars <lars50593@gmail.com>
2020-08-24 23:43:15 -0600

    Countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo no puede contener mas de 50 caracteres")]
+        [Required]
+        public string Name { get; set; }
+    }
+
+
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //Propiedad para poder mapear tablas.
+        public DbSet<Country> Countries { get; set; }
+
+        // funcion de indice para que no se puedan repetir los paises
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+        }
+    }
+}
